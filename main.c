7428d72a8700210993feef597271f981b4def41a
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-28 19:28:08 +0200

    Hello, World!

---
+++ main.c
@@ -0,0 +1,33 @@
+#define SETUP
+#define UPDATE
+#define RENDER
+#include "display.c"
+#define MATRIX_IMPLEMENTATION
+#include "Matrix.h"
+#define VEC2_IMPLEMENTATION
+#include "Vec2.h"
+#define VEC3_IMPLEMENTATION
+#include "Vec3.h"
+#define RENDER_SHAPES_SDL_IMPLEMENTATION
+#include "render_shapes_SDL.h"
+
+void setup(void)
+{
+    game_is_running = 0;
+
+    printf("Hello, World!\n");
+    
+}
+
+void update(void)
+{
+    
+
+    
+}
+
+void render(void)
+{
+    
+
+}
